38ebd500739bdc924f9611537e2a666a42772039
Benitez Gonzalo <126210657+gonzalo12121212@users.noreply.github.com>
2023-09-26 22:01:58 -0300

    Menu_con_Funciones.py

--- Menu_con_Funciones.py
+++ Menu_con_Funciones.py
@@ -1,5 +1,5 @@
-import Funciones 
-import Menu_de_Opciones
+from Funciones import*
+from Menu_de_Opciones import*
 
 while True:
 
@@ -8,7 +8,7 @@ while True:
     print("*** Menu de Opciones ***")
     print("")
 
-    Menu_de_Opciones.mostrar_menu()
+    mostrar_menu()
 
     try:
         opcion = input("Selecciona una opción: ")
@@ -16,43 +16,43 @@ while True:
         print("Reintente ingresando un valor numerico, porfavor")
 
     if opcion == '1':
-        Menu_de_Opciones.opcion_1()
+        opcion_1()
         break
 
     elif opcion == '2':
-        Menu_de_Opciones.opcion_2()
+        opcion_2()
         break
 
     elif opcion == '3':
-        Menu_de_Opciones.opcion_3()
+        opcion_3()
         break
 
     elif opcion == '4':
-        Menu_de_Opciones.opcion_4()
+        opcion_4()
         break
 
     elif opcion == '5':
-        Menu_de_Opciones.opcion_5()
+        opcion_5()
         break
 
     elif opcion == '6':
-        Menu_de_Opciones.opcion_6()
+        opcion_6()
         break
 
     elif opcion == '7':
-        Menu_de_Opciones.opcion_7()
+        opcion_7()
         break
 
     elif opcion == '8':
-        Menu_de_Opciones.opcion_8()
+        opcion_8()
         break
 
     elif opcion == '9':
-        Menu_de_Opciones.opcion_9()
+        opcion_9()
         break
 
     elif opcion == '10':
-        Menu_de_Opciones.opcion_10()
+        opcion_10()
         break
 
     elif opcion == '11':
