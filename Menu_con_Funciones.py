d7eb953864dc3f550ac2a27217789836373d0377
Benitez Gonzalo <126210657+gonzalo12121212@users.noreply.github.com>
2023-10-03 22:38:58 -0300

    Update Menu_con_Funciones.py

--- Menu_con_Funciones.py
+++ Menu_con_Funciones.py
@@ -14,49 +14,50 @@ while True:
         opcion = input("Selecciona una opción: ")
     except:
         print("Reintente ingresando un valor numerico, porfavor")
+    try:
+        match opcion :
+            case '1':
+                opcion_1()
+                break
 
-    if opcion == '1':
-        opcion_1()
-        break
+            case '2':
+                opcion_2()
+                break
 
-    elif opcion == '2':
-        opcion_2()
-        break
+            case '3':
+                opcion_3()
+                break
 
-    elif opcion == '3':
-        opcion_3()
-        break
+            case '4':
+                opcion_4()
+                break
 
-    elif opcion == '4':
-        opcion_4()
-        break
+            case '5':
+                opcion_5()
+                break
 
-    elif opcion == '5':
-        opcion_5()
-        break
+            case '6':
+                opcion_6()
+                break
 
-    elif opcion == '6':
-        opcion_6()
-        break
+            case '7':
+                opcion_7()
+                break
 
-    elif opcion == '7':
-        opcion_7()
-        break
+            case '8':
+                opcion_8()
+                break
 
-    elif opcion == '8':
-        opcion_8()
-        break
+            case '9':
+                opcion_9()
+                break
 
-    elif opcion == '9':
-        opcion_9()
-        break
+            case '10':
+                opcion_10()
+                break
 
-    elif opcion == '10':
-        opcion_10()
-        break
+            case '11':    
+                break
 
-    elif opcion == '11':
-        break
-
-    else:
+    except:
         print("Opción inválida. Inténtalo de nuevo.") 
